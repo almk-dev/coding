e1b93295f9d0ed704ba9446542baf2187728667e
Abdul Malek <me@almk.dev>
2025-04-05 12:40:21 -0700

    add test

--- capital-rx/src/test_report.py
+++ capital-rx/src/test_report.py
@@ -1,7 +1,20 @@
+import report
+from pathlib import Path
+
+TEST_DATA_DIR = Path.cwd() / "data"
+
 def test_report():
     """
     Assert generate_nadac_top_price_change_report(2020, 10) output exactly matches data/top_10_2020.txt.
     """
+    test_file_name = "top_10_2020.txt"
+    test_file_full_path = TEST_DATA_DIR / test_file_name
+    with open(test_file_full_path) as file:
+        expected_lines = file.readlines()
 
-    # TODO: Implement data/top_10_2020.txt test here
-    assert False
+    actual = report.generate_nadac_top_price_change_report(2020, 10)
+    actual_lines = actual.split('\n')
+
+    assert len(expected_lines) == len(actual_lines)
+    for i in range(len(expected_lines)):
+        assert expected_lines[i] == actual_lines[i]
